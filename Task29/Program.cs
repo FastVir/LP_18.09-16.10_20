c2e311eb40b4ed8735fa2f394fe91bd184688ea2
Dimitry Timofeev <fasvir32gar@gmail.com>
2023-10-04 16:17:14 +0300

    Add Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -6,9 +6,43 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] creatArray(int size)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
+    int[] arr = new int[size];
+    Random rnd = new Random();
 
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
 }
 
-Console.WriteLine()
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int Promt(string Message)
+{
+    Console.WriteLine(Message);
+     int input = Convert.ToInt32(Console.ReadLine());
+    // string input = Console.ReadLine();
+    // int result = Convert.ToInt32(input);
+    return input;   
+}
+
+
+int length = Promt("Введите количество элементов массива: ");
+int min = Promt("Введите минимальное значение: ");
+int max = Promt("Введите максимальное значние:");
+int[] array = CreateArrayRndInt(length, min, max); 
+PrintArray(array);
+

922f247db691d1966d08f4e970e02c18bfc91e19
Dimitry Timofeev <fasvir32gar@gmail.com>
2023-10-12 21:11:40 +0300

    Task46,53,55

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -6,3 +6,32 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
+int[,] CreatMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreatMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
--- Task55/Program.cs
+++ Task55/Program.cs
@@ -3,3 +3,86 @@
 // невозможно, программа должна вывести сообщение для
 // пользователя.
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        //Console.WriteLine("  |");
+        Console.WriteLine();
+    }
+}
+
+//  1 5 7
+//  7 8 9
+//  4 5 9
+
+//  1 7 4
+//  5 8 5
+//  7 9 9
+
+//  1 7 4
+//  5 8 5
+//  7 9 9
+
+
+// 5(1,0) 7(2,0) 9(2,1) 
+
+void ReplaceRowsColumns2(int[,] matrix) // 2й более оптимальный метод замены строк на столбцы
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+
+void ReplaceRowsColumns(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, -100, 100);
+PrintMatrix(array2d);
+ReplaceRowsColumns(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+ReplaceRowsColumns2(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
